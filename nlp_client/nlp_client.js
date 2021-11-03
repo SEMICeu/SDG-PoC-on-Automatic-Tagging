fd1bd7d3151100266cc7f13f7308fae7dd99fd8c
Emidio Stani <emidiostani@gmail.com>
2021-11-03 22:39:08 +0100

    check if element is mandatory

--- nlp_client/nlp_client.js
+++ nlp_client/nlp_client.js
@@ -4,18 +4,31 @@ function getArray() {
   return $.getJSON("metatags.json");
 }
 
+var notexist = [];
 getArray().done(function (json) {
   // now you can use json
-  var questions = [];
   $.each(json.items, function (key, val) {
     var name = val.name;
     if ($("meta[name='" + name + "']").length) {
-      console.log(name + "exist");
+      console.log(name + " exist");
     } else {
-      console.log(name + "does not exist");
+      console.log(name + " does not exist");
+      var mandatory = val.mandatory;
+      if (mandatory) {
+        console.log(name + " is mandatory");
+        notexist.push(name);
+      } else {
+        console.log(name + " is not mandatory");
+      }
     }
   });
+  callapi(notexist);
 });
 
-var description = $("meta[name=description]").attr("content");
-$("#output").html(description);
+function callapi(notexistArray) {
+  $.each(notexistArray, function (i, val) {
+    console.log(val + " is due");
+  });
+  var description = $("meta[name=description]").attr("content");
+  $("#output").html(description);
+}

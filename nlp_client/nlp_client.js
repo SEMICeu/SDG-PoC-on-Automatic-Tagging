02db39cac06f6e8e9eb2a256e224ac5927f9d6f7
Emidio Stani <emidiostani@gmail.com>
2021-11-03 21:54:11 +0100

    parsing json

--- nlp_client/nlp_client.js
+++ nlp_client/nlp_client.js
@@ -1,2 +1,15 @@
+function getArray() {
+  return $.getJSON("metatags.json");
+}
+
+getArray().done(function (json) {
+  // now you can use json
+  var questions = [];
+  $.each(json.items, function (key, val) {
+    var name = val.name;
+    console.log(name);
+  });
+});
+
 var description = $("meta[name=description]").attr("content");
 $("#output").html(description);

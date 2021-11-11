3c105f91ea8bf5b1be83b7e83cbcf823a5a011b3
emidiostani <emidiostani@gmail.com>
2021-11-11 22:23:22 +0100

    enabled swagger ui

--- api/src/nlp_api/run.py
+++ api/src/nlp_api/run.py
@@ -9,7 +9,7 @@ def create_app():
     abs_file_path = os.path.abspath(os.path.dirname(__file__))
     openapi_path = os.path.join(abs_file_path, "../", "../", "spec")
     app = connexion.FlaskApp(
-        __name__, specification_dir=openapi_path, options={"swagger_ui": True, "serve_spec": False}
+        __name__, specification_dir=openapi_path, options={"swagger_ui": True, "serve_spec": True}
     )
     app.add_api("nlpapi.yaml", strict_validation=True)
     flask_app = app.app

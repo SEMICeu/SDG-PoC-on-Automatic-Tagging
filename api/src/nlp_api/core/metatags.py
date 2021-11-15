f72c090597a930e17514aa1045836cecb8b33405
emidiostani <emidiostani@gmail.com>
2021-11-15 23:43:25 +0100

    test api automatic deploy

--- api/src/nlp_api/core/metatags.py
+++ api/src/nlp_api/core/metatags.py
@@ -13,7 +13,7 @@ def enhance(request):
     metatags_in_request = request.metatags
     metatags_in_response = []
     for i in metatags_in_request:
-        metatag = MetaTag(name=i,value="Test")
+        metatag = MetaTag(name=i,value="Test1")
         metatags_in_response.append(metatag)
 
     response = EnhanceResponse(metatags=metatags_in_response)

c5917b3e01576cb7a9f5b08d3d02decafde53585
emidiostani <emidiostani@gmail.com>
2021-11-16 23:01:25 +0100

    reading from mysql

--- api/src/nlp_api/core/metatags.py
+++ api/src/nlp_api/core/metatags.py
@@ -2,16 +2,53 @@ import json
 
 from ..web.models import Status, EnhanceRequest, EnhanceResponse, MetaTag
 
+import yaml
+import mysql.connector
+from mysql.connector import Error
+from pathlib import Path
+
+def get_config():
+
+
+    my_path = Path(__file__).resolve()  # resolve to get rid of any symlinks
+    config_path = my_path.parent / 'config.yaml'
+    with config_path.open() as config_file:
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+    return config
 
 def get_status():
-    status = Status(True)
-    return status
+    config = get_config()
+    myhost = config['mysql']['host']
+    mydb = config['mysql']['db']
+    myuser = config['mysql']['username']
+    mypass = config['mysql']['password']
+    try:
+        connection = mysql.connector.connect(host=myhost,
+                                         database=mydb,
+                                         user=myuser,
+                                         password=mypass)
+
+        select_query = "SELECT status FROM status"
+        
+        cursor = connection.cursor()
+        cursor.execute(select_query)
+        record = cursor.fetchone()
+        print("result " + str(record[0]))
+        status = Status(bool(record[0]))
+        return status
+    
+    except mysql.connector.Error as error:
+        print("Failed to create table in MySQL: {}".format(error))
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
 
 def enhance(request):
-
-
     metatags_in_request = request.metatags
     metatags_in_response = []
+
     for i in metatags_in_request:
         metatag = MetaTag(name=i,value="Test1")
         metatags_in_response.append(metatag)

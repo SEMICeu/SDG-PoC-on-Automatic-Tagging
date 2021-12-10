5c6ec35c875d771bf8b3772bb31adb7132dd278d
emidiostani <emidiostani@gmail.com>
2021-12-10 12:17:46 +0100

    added not implemented and improved logging

--- nlp_client/nlp_client.js
+++ nlp_client/nlp_client.js
@@ -4,9 +4,9 @@ function getConfig() {
   return $.getJSON("config.json");
 }
 
-function insertMetaTag(name, value) {
-  console.log("inserting meta: name " + name + " value: " + value);
-  $("head").append('<meta name="' + name + '" content="' + value + '">');
+function insertMetaTag(name, value, notimplemented) {
+    console.log("[NLP-POC] inserting meta: name " + name + " value: " + value);
+    $("head").append('<meta name="' + name + '" content="' + value + '">');
 }
 
 function get_tld(url) {
@@ -20,34 +20,34 @@ function getParameters(json) {
   // now you can use json
 
   var elementToExtract = json.page.elementToExtract;
-  console.log("elementToExtract: " + elementToExtract);
+  console.log("[NLP-POC] elementToExtract: " + elementToExtract);
   var page_url = window.location.href;
-  console.log("page url: " + page_url);
+  console.log("[NLP-POC] page url: " + page_url);
   var tld = get_tld(page_url);
-  console.log("tld: " + tld);
+  console.log("[NLP-POC] tld: " + tld);
 
   var metatagsArray = [];
   var includeOptionalTags = json.page.includeOptionalTags;
   $.each(json.metatags, function (key, val) {
     var name = val.name;
     if ($("meta[name='" + name + "']").length) {
-      console.log(name + " exist");
+      console.log("[NLP-POC] " + name + " exist");
     } else {
-      console.log(name + " does not exist");
+      console.log("[NLP-POC] " + name + " does not exist");
 
       var mandatory = val.mandatory;
       if (mandatory) {
-        console.log(name + " is mandatory");
+        console.log("[NLP-POC] " + name + " is mandatory");
         if (val.hasOwnProperty("expectedValue")) {
-          console.log(val.expectedValue + " is expected");
-          insertMetaTag(name, val.expectedValue);
+          console.log("[NLP-POC] " + val.expectedValue + " is expected");
+          insertMetaTag(name, val.expectedValue, );
         } else if (val.hasOwnProperty("mapValue")) {
           if (name === "DC.ISO3166") {
             if (val.listOfValues.includes(tld)) {
               insertMetaTag(name, tld);
             } else {
               var obj = val.mapValue.find((o) => o.name === tld);
-              console.log(obj);
+              console.log("[NLP-POC] " + obj);
               if (obj !== undefined) {
                 tld = obj.value;
                 insertMetaTag(name, tld);
@@ -61,22 +61,22 @@ function getParameters(json) {
         }
       } else {
         if (includeOptionalTags) {
-          console.log(name + " is optional but requested");
+          console.log("[NLP-POC] " + name + " is optional but requested");
           if (val.hasOwnProperty("expectedValue")) {
-            console.log(val.expectedValue + " is expected");
+            console.log("[NLP-POC] " + val.expectedValue + " is expected");
             insertMetaTag(name, val.expectedValue);
           } else {
             metatagsArray.push(name);
           }
         } else {
-          console.log(name + " is not mandatory and not requested");
+          console.log("[NLP-POC] " + name + " is not mandatory and not requested");
         }
       }
     }
   });
 
   $.each(metatagsArray, function (i, val) {
-    console.log(val + " is due");
+    console.log("[NLP-POC] " + val + " is due");
   });
   callApi(page_url, elementToExtract, metatagsArray, json);
 }
@@ -84,28 +84,28 @@ function getParameters(json) {
 function callApi(page_url, elementToExtract, metatagsArray, json) {
   
   var base_url = json.api.baseurl;
-  console.log("base url: " + base_url);
+  console.log("[NLP-POC] base_url: " + base_url);
   var canenhance_url = base_url + "/" + json.api.operations[0].name;
-  console.log("canenhance_url: " + canenhance_url);
+  console.log("[NLP-POC] canenhance_url: " + canenhance_url);
   $.ajax({
     url: canenhance_url,
     method: json.api.operations[0].method,
     timeout: json.api.operations[0].timeout
   }).done(function (response1) {
-    console.log(response1);
+    console.log("[NLP-POC] response: " + response1);
     if (response1.status) {
       var enhance_url = base_url + "/" + json.api.operations[1].name;
-      console.log("enhance_url: " + enhance_url);
+      console.log("[NLP-POC] enhance_url: " + enhance_url);
 
       var api_payload = json.api.operations[1].payload;
-      console.log("api payload: " + api_payload);
+      console.log("[NLP-POC] api payload: " + api_payload);
       var text = $(elementToExtract).text().trim().replace(/\s+/g, " ");
-      console.log("text: " + text);
+      console.log("[NLP-POC] text: " + text);
       var request = api_payload
         .replace("$METATAGS$", JSON.stringify(metatagsArray))
         .replace("$URL$", JSON.stringify(page_url))
         .replace("$TEXT$", JSON.stringify(text));
-      console.log(request);
+      console.log("[NLP-POC] request: " + request);
       $.ajax({
         url: enhance_url,
         method: json.api.operations[1].method,
@@ -115,21 +115,22 @@ function callApi(page_url, elementToExtract, metatagsArray, json) {
           "Content-Type": "application/json"
         }
       }).done(function (response2) {
-        console.log(response2);
+        console.log("[NLP-POC] " + response2);
         response2.metatags.forEach((element) => {
-          insertMetaTag(element.name, element.value);
+          if(element.value !=  json.api.operations[1].notimplemented)
+            insertMetaTag(element.name, element.value);
         });
       })
       .fail(function (jqXHR, textStatus) {
         if (textStatus === "timeout") {
-          console.log("API timeout");
+          console.log("[NLP-POC] API timeout");
         }
       });
     }
   })
   .fail(function (jqXHR, textStatus) {
     if (textStatus === "timeout") {
-      console.log("API timeout");
+      console.log("[NLP-POC] API timeout");
     }
   });
 }

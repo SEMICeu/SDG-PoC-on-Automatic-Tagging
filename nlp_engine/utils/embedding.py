a308668b9b660e5dac9d9ac99c74f989020e197d
LouisMatha <79630469+LouisMatha@users.noreply.github.com>
2021-12-12 14:23:18 +0100

    Update embedding.py

--- nlp_engine/utils/embedding.py
+++ nlp_engine/utils/embedding.py
@@ -1,6 +1,7 @@
 import numpy as np
 from gensim.models import Word2Vec
 import os
+from pathlib import Path
 
 def embedding(text: str):
     """
@@ -10,9 +11,9 @@ def embedding(text: str):
     
     This function returns the embeddings thanks to the language models
     """
-    dir_path = os.path.dirname(os.path.realpath(__file__).parent).replace("\\", "/")
+    dir_path = Path(os.path.dirname(os.path.realpath(__file__)).replace("\\", "/"))
 
-    path_to_model = dir_path + "/language_model/Word2Vec.model"
+    path_to_model = str(dir_path.parent.absolute()) + "/language_model/Word2Vec.model"
     model = Word2Vec.load(path_to_model)
     w2v = dict(zip(model.wv.index2word, model.wv.vectors))
     dim = len(next(iter(w2v.values())))

833c8c1b4e5945f12f2c9d5a7edb5f85d9d6fe73
emidiostani <emidiostani@gmail.com>
2021-12-13 12:56:48 +0100

    added space

--- nlp_client/nlp_client.js
+++ nlp_client/nlp_client.js
@@ -114,8 +114,8 @@ function callApi(page_url, metatagsArray, json) {
       }).done(function (response2) {
         console.log("[NLP-POC] Response: " + JSON.stringify(response2));
         response2.metatags.forEach((element) => {
-          if(element.value !=  json.api.operations[1].notimplemented || element.value !=  json.api.operations[1].notfound || element.value !="")
-            insertMetaTag(element.name, element.value);
+          if(element.value !=  json.api.operations[1].notimplemented || element.value !=  json.api.operations[1].notfound || element.value != "")  
+          insertMetaTag(element.name, element.value);
         });
       })
       .fail(function (jqXHR, textStatus) {

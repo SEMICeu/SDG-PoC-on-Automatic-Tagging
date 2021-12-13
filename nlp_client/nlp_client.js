fc32fb807a1c4c949e18f903ca99966058da7363
emidiostani <emidiostani@gmail.com>
2021-12-13 12:45:34 +0100

    added condiiton on empty values

--- nlp_client/nlp_client.js
+++ nlp_client/nlp_client.js
@@ -114,7 +114,7 @@ function callApi(page_url, metatagsArray, json) {
       }).done(function (response2) {
         console.log("[NLP-POC] Response: " + JSON.stringify(response2));
         response2.metatags.forEach((element) => {
-          if(element.value !=  json.api.operations[1].notimplemented || element.value !=  json.api.operations[1].notfound)
+          if(element.value !=  json.api.operations[1].notimplemented || element.value !=  json.api.operations[1].notfound || element.value !="")
             insertMetaTag(element.name, element.value);
         });
       })

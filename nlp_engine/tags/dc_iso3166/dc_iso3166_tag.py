6c6a910fac93a6d582581542fd0ebb7632c91a50
LouisMatha <79630469+LouisMatha@users.noreply.github.com>
2022-02-01 11:08:30 +0100

    Update dc_iso3166_tag

--- nlp_engine/tags/dc_iso3166/dc_iso3166_tag.py
+++ nlp_engine/tags/dc_iso3166/dc_iso3166_tag.py
@@ -21,12 +21,14 @@ def dc_iso3166_tag(url):
     db = Database()
 
     country_tag = db.lookup_code(country)
+    try:
+        pycountry_result = pycountry.countries.get(name=country_tag)
 
-    pycountry_result = pycountry.countries.get(name=country_tag)
+        iso3166_tag = pycountry_result.alpha_2
 
-    iso3166_tag = pycountry_result.alpha_2
-
-    if iso3166_tag == "":
+        if iso3166_tag == "":
+            iso3166_tag = "404"
+    except:
         iso3166_tag = "404"
 
     return iso3166_tag
